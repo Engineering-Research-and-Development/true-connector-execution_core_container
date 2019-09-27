0857499a5dab6e966594d9110d2a5cf4cd08db97
Gabriele De Luca <gabriele.deluca@hotmail.com>
2019-09-27 17:08:15 +0200

    SSL verification disabled

--- src/main/java/it/eng/idsa/businesslogic/service/impl/CommunicationServiceImpl.java
+++ src/main/java/it/eng/idsa/businesslogic/service/impl/CommunicationServiceImpl.java
@@ -5,6 +5,17 @@ package it.eng.idsa.businesslogic.service.impl;
 
 
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.ParseException;
@@ -24,7 +35,56 @@ import it.eng.idsa.businesslogic.service.CommunicationService;
 @Transactional
 public class CommunicationServiceImpl implements CommunicationService {
 
-	
+	static {
+	    disableSslVerification();
+	}
+
+	private static void disableSslVerification() {
+	    try
+	    {
+	        // Create a trust manager that does not validate certificate chains
+	        TrustManager[] trustAllCerts = new TrustManager[] {new X509TrustManager() {
+				
+				@Override
+				public X509Certificate[] getAcceptedIssuers() {
+					// TODO Auto-generated method stub
+					return null;
+				}
+				
+				@Override
+				public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+					// TODO Auto-generated method stub
+					
+				}
+	        }
+	        };
+	       
+	        // Install the all-trusting trust manager
+	        SSLContext sc = SSLContext.getInstance("SSL");
+	        sc.init(null, trustAllCerts, new java.security.SecureRandom());
+	        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+	        // Create all-trusting host name verifier
+	        HostnameVerifier allHostsValid = new HostnameVerifier() {
+	            public boolean verify(String hostname, SSLSession session) {
+	                return true;
+	            }
+	        };
+
+	        // Install the all-trusting host verifier
+	        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+	    } catch (NoSuchAlgorithmException e) {
+	        e.printStackTrace();
+	    } catch (KeyManagementException e) {
+	        e.printStackTrace();
+	    }
+	}
 	@Override
 	public String sendData(String endpoint, HttpEntity data) {
 		// TODO Auto-generated method stub
