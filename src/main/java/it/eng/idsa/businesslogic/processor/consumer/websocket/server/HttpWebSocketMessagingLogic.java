e67da6869d4c93e957340f2eb8d17d3e3041c9de
ascatox <ascatox@me.com>
2020-03-26 13:57:36 +0100

    Version To Release in main repo.

--- src/main/java/it/eng/idsa/businesslogic/processor/consumer/websocket/server/HttpWebSocketMessagingLogic.java
+++ src/main/java/it/eng/idsa/businesslogic/processor/consumer/websocket/server/HttpWebSocketMessagingLogic.java
@@ -14,8 +14,6 @@ import java.nio.charset.StandardCharsets;
  */
 public class HttpWebSocketMessagingLogic {
     private static final Logger logger = LogManager.getLogger(HttpWebSocketMessagingLogic.class);
-    private static final String CLOSURE_FRAME = "�normal closure";
-    private static final String END_BINARY_FRAME_SEPARATOR = "�normal-IDS-ENG-SEPARATOR the-last-frame";
 
     private WebSocketServerConfiguration webSocketServerConfiguration;
     private static HttpWebSocketMessagingLogic instance;
@@ -30,16 +28,16 @@ public class HttpWebSocketMessagingLogic {
         return instance;
     }
 
-    //Duplicate code fragment @See InputStreamSocketListenerServer onMessage()
+    // TODO Duplicate code fragment @See InputStreamSocketListenerServer onMessage()
     public void onMessage(Session session, byte[] message) {
         String receivedMessage = new String(message, StandardCharsets.UTF_8);
-       if (receivedMessage.equals(CLOSURE_FRAME)) {
+       if (receivedMessage.equals(InputStreamSocketListenerServer.CLOSURE_FRAME)) {
             // The last frame is received - skip this frame
             // This indicate that Client WebSocket now is closed
         } else {
             // Put the received frame in the frameBuffer
             webSocketServerConfiguration.frameBufferWebSocket().add(message.clone());
-            if (receivedMessage.equals(END_BINARY_FRAME_SEPARATOR)) {
+            if (receivedMessage.equals(InputStreamSocketListenerServer.END_BINARY_FRAME_SEPARATOR)) {
                 ResponseMessageSendPartialServer responseMessageSendPartialServer = webSocketServerConfiguration.responseMessageSendPartialWebSocket();
                 responseMessageSendPartialServer.setup(session);
                 Thread fileRecreatorBeanThread = new Thread(responseMessageSendPartialServer, "ResponseMessageSendPartialServer");
