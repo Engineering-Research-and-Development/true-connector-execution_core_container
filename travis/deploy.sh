225aa77c51f8e4ad775ec1ff79ff538681e77ecc
Gabriele De Luca <35374403+gabrieledeluca@users.noreply.github.com>
2020-07-03 16:10:32 +0200

    Update deploy.sh

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -2,4 +2,5 @@
 echo "Deploy to Docker Hub"
 docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
 docker tag rdlabengpa/execution_core_container_bl rdlabengpa/execution_core_container_bl:develop
+docker rmi rdlabengpa/execution_core_container_bl:latest
 docker push rdlabengpa/execution_core_container_bl
