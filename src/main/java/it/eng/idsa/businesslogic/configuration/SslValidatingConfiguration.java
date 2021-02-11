c6a444f2a38c158131a91e2bd7ce2ccb41248cac
Vojkan Bukumiric <vojkanbukumiric@gmail.com>
2021-02-11 10:51:55 +0100

    Removed TODO comments

--- src/main/java/it/eng/idsa/businesslogic/configuration/SslValidatingConfiguration.java
+++ src/main/java/it/eng/idsa/businesslogic/configuration/SslValidatingConfiguration.java
@@ -39,20 +39,15 @@ public class SslValidatingConfiguration {
 
 				@Override
 				public X509Certificate[] getAcceptedIssuers() {
-					// TODO Auto-generated method stub
 					return null;
 				}
 
 				@Override
 				public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-					// TODO Auto-generated method stub
-
 				}
 
 				@Override
 				public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-					// TODO Auto-generated method stub
-
 				}
 			} };
 
