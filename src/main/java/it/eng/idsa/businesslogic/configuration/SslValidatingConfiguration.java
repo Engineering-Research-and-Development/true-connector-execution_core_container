98a8180bb42a1f63dab4da5bdb26ca5a68f993ce
vojkanBukumiric91 <60774768+vojkanBukumiric91@users.noreply.github.com>
2021-02-11 10:54:35 +0100

    Disable ssl validation optional (#22)
    * SSL validating external calls is optional
    
    * Removed TODO comments

---
+++ src/main/java/it/eng/idsa/businesslogic/configuration/SslValidatingConfiguration.java
@@ -0,0 +1,75 @@
+package it.eng.idsa.businesslogic.configuration;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SslValidatingConfiguration {
+
+	private static final Logger logger = LogManager.getLogger(SslValidatingConfiguration.class);
+
+	public SslValidatingConfiguration(
+			@Value("${application.disableSslVerification:false}") boolean disableSslVerification) {
+		if (disableSslVerification) {
+			logger.info("Disabling ssl validation");
+			disableSslVerification();
+		} else {
+			logger.info("Using default ssl validation");
+		}
+	}
+
+	private void disableSslVerification() {
+		try {
+			// Create a trust manager that does not validate certificate chains
+			TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+
+				@Override
+				public X509Certificate[] getAcceptedIssuers() {
+					return null;
+				}
+
+				@Override
+				public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+				}
+
+				@Override
+				public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+				}
+			} };
+
+			// Install the all-trusting trust manager
+			SSLContext sc = SSLContext.getInstance("SSL");
+			sc.init(null, trustAllCerts, new java.security.SecureRandom());
+			HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+			// Create all-trusting host name verifier
+			HostnameVerifier allHostsValid = new HostnameVerifier() {
+				public boolean verify(String hostname, SSLSession session) {
+					return true;
+				}
+			};
+
+			// Install the all-trusting host verifier
+			HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (KeyManagementException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
