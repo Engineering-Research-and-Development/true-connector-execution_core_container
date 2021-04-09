69f1f061111cd30003a8581ea6475bfaa743e631
igor.balog <igor.balog@eng.it>
2021-04-09 11:33:57 +0200

    Refactored code to use KeystoreProvider
    Centralized place for keystore and truststore management

--- src/main/java/it/eng/idsa/businesslogic/configuration/SslValidatingConfiguration.java
+++ src/main/java/it/eng/idsa/businesslogic/configuration/SslValidatingConfiguration.java
@@ -1,117 +1,57 @@
 package it.eng.idsa.businesslogic.configuration;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.security.InvalidAlgorithmParameterException;
 import java.security.KeyManagementException;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
-import java.security.cert.PKIXParameters;
-import java.security.cert.TrustAnchor;
 import java.security.cert.X509Certificate;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
+import javax.annotation.PostConstruct;
 import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
-import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
 import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+
+import it.eng.idsa.businesslogic.service.impl.KeystoreProvider;
 
 @Configuration
 public class SslValidatingConfiguration {
 	
 	private static final Logger logger = LogManager.getLogger(SslValidatingConfiguration.class);
 
-	private KeyStore keystore;
-	private KeyStore trustManagerKeyStore;
+	@Autowired
+	private KeystoreProvider keystoreProvider;
+	@Value("${application.disableSslVerification:false}") 
+	private boolean disableSslVerification;
 	
-	public SslValidatingConfiguration(
-				@Value("${application.disableSslVerification:false}") boolean disableSslVerification,
-				@Value("${application.targetDirectory}") Path targetDirectory,
-				@Value("${application.keyStoreName}") String keyStoreName, 
-				@Value("${application.keyStorePassword}") String keyStorePassword, 
-				@Value("${application.trustStoreName}") String trustStoreName, 
-				@Value("${application.trustStorePassword}") String trustStorePwd) 
-					throws KeyManagementException, NoSuchAlgorithmException, UnrecoverableKeyException {
+	@PostConstruct
+	public void setupSSL() throws KeyManagementException, NoSuchAlgorithmException {
 		if (disableSslVerification) {
 			logger.info("Disabling ssl validation");
 			disableSslVerification();
 		} else {
 			logger.info("Using default ssl validation");
-			setTrustStore(targetDirectory, keyStoreName, keyStorePassword, trustStoreName, trustStorePwd);
+			setTrustStore();
 		}
 	}
 
 	/**
 	 * Needed because URLJwsProvider uses URL.openConnection() and we need to set trustStore
-	 * @param targetDirectory
-	 * @param keyStoreName
-	 * @param keyStorePassword
-	 * @param trustStoreName
-	 * @param trustStorePwd
 	 * @throws KeyManagementException
 	 * @throws NoSuchAlgorithmException
-	 * @throws UnrecoverableKeyException
 	 */
-	private void setTrustStore(Path targetDirectory, String keyStoreName, String keyStorePassword, 
-			String trustStoreName, String trustStorePwd) throws KeyManagementException, NoSuchAlgorithmException, UnrecoverableKeyException {
+	private void setTrustStore() throws NoSuchAlgorithmException, KeyManagementException {
+		logger.info("Setting up truststore");
 		SSLContext sslCtx = SSLContext.getInstance("TLS");
-		KeyManagerFactory kmf = null;
-		TrustManagerFactory trustFactory = null;
-		try {
-			InputStream jksKeyStoreInputStream = Files.newInputStream(targetDirectory.resolve(keyStoreName));
-			keystore = KeyStore.getInstance("JKS");
-			logger.info("Loading key store: " + keyStoreName);
-			keystore.load(jksKeyStoreInputStream, keyStorePassword.toCharArray());
-			kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-			kmf.init(keystore, keyStorePassword.toCharArray());
-			
-			logger.info("Loading trust store: " + trustStoreName);
-			InputStream jksTrustStoreInputStream = Files.newInputStream(targetDirectory.resolve(trustStoreName));
-			trustManagerKeyStore = KeyStore.getInstance("JKS");
-			trustManagerKeyStore.load(jksTrustStoreInputStream, trustStorePwd.toCharArray());
-			if(logger.isDebugEnabled()) {
-				PKIXParameters params;
-				try {
-					params = new PKIXParameters(trustManagerKeyStore);
-					Set<TrustAnchor> trustAnchors = params.getTrustAnchors();
-					List<Certificate> certificates = trustAnchors.stream()
-							.map(TrustAnchor::getTrustedCert)
-							.collect(Collectors.toList());
-					logger.debug("Cert chain: " + certificates);
-				} catch (InvalidAlgorithmParameterException e) {
-					logger.error("Error while trying to read trusted certificates");
-				}
-			}
-			List<String> aliases = Collections.list(trustManagerKeyStore.aliases());
-			logger.info("Trusted certificate aliases: {}", aliases);
-			trustFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-            trustFactory.init(trustManagerKeyStore);
-			
-			logger.info("LOADED CA CERT: " + trustManagerKeyStore.getCertificate("ca"));
-			jksKeyStoreInputStream.close();
-			jksTrustStoreInputStream.close();
-		} catch (IOException | KeyStoreException | NoSuchAlgorithmException | CertificateException e) {
-			logger.error("Error while loading keystore and truststore, {}", e);
-		}
-		
-		sslCtx.init(kmf.getKeyManagers(), trustFactory.getTrustManagers(), new java.security.SecureRandom());
+		sslCtx.init(keystoreProvider.getKeystoreFactory().getKeyManagers(), 
+				keystoreProvider.getTrustManagerFactory().getTrustManagers(), 
+				new java.security.SecureRandom());
 		HttpsURLConnection.setDefaultSSLSocketFactory(sslCtx.getSocketFactory());
 	}
 	
--- src/main/java/it/eng/idsa/businesslogic/service/impl/KeystoreProvider.java
+++ src/main/java/it/eng/idsa/businesslogic/service/impl/KeystoreProvider.java
@@ -4,14 +4,24 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.Key;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
+import java.security.cert.PKIXParameters;
+import java.security.cert.TrustAnchor;
 import java.security.cert.X509Certificate;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.TrustManagerFactory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,6 +37,8 @@ public class KeystoreProvider {
 	private KeyStore trustManagerKeyStore;
 	private String keyStorePwd;
 	private String keystoreAlias;
+	private KeyManagerFactory kmf;
+	private TrustManagerFactory trustFactory;
 	
 	public KeystoreProvider(@Value("${application.targetDirectory}") Path targetDirectory,
 							@Value("${application.keyStoreName}") String keyStoreName, 
@@ -40,24 +52,52 @@ public class KeystoreProvider {
 		
 		try {
 			InputStream jksKeyStoreInputStream = Files.newInputStream(targetDirectory.resolve(keyStoreName));
-			InputStream jksTrustStoreInputStream = Files.newInputStream(targetDirectory.resolve(trustStoreName));
-			
 			keystore = KeyStore.getInstance("JKS");
-			trustManagerKeyStore = KeyStore.getInstance("JKS");
-			
 			logger.info("Loading key store: " + keyStoreName);
-			logger.info("Loading trust store: " + keyStoreName);
 			keystore.load(jksKeyStoreInputStream, keyStorePassword.toCharArray());
-			trustManagerKeyStore.load(jksTrustStoreInputStream, trustStorePwd.toCharArray());
-			java.security.cert.Certificate[] certs = trustManagerKeyStore.getCertificateChain("ca");
-			logger.info("Cert chain: " + Arrays.toString(certs));
+			kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+			kmf.init(keystore, keyStorePassword.toCharArray());
 			
-			logger.info("LOADED CA CERT: " + trustManagerKeyStore.getCertificate("ca"));
+			logger.info("Loading trust store: " + trustStoreName);
+			InputStream jksTrustStoreInputStream = Files.newInputStream(targetDirectory.resolve(trustStoreName));
+			trustManagerKeyStore = KeyStore.getInstance("JKS");
+			trustManagerKeyStore.load(jksTrustStoreInputStream, trustStorePwd.toCharArray());
+			if(logger.isDebugEnabled()) {
+				PKIXParameters params;
+				try {
+					params = new PKIXParameters(trustManagerKeyStore);
+					Set<TrustAnchor> trustAnchors = params.getTrustAnchors();
+					List<Certificate> certificates = trustAnchors.stream()
+							.map(TrustAnchor::getTrustedCert)
+							.collect(Collectors.toList());
+					logger.debug("Cert chain: " + certificates);
+				} catch (InvalidAlgorithmParameterException e) {
+					logger.error("Error while trying to read trusted certificates");
+				}
+			}
+			List<String> aliases = Collections.list(trustManagerKeyStore.aliases());
+			logger.info("Trusted certificate aliases: {}", aliases);
+			trustFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            trustFactory.init(trustManagerKeyStore);
+			
+			kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+			kmf.init(keystore, keyStorePassword.toCharArray());
+			trustFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+			trustFactory.init(trustManagerKeyStore);
+			
 			jksKeyStoreInputStream.close();
 			jksTrustStoreInputStream.close();
-		} catch (IOException | KeyStoreException | NoSuchAlgorithmException | CertificateException e) {
+		} catch (IOException | KeyStoreException | NoSuchAlgorithmException | CertificateException | UnrecoverableKeyException e) {
 			logger.error("Error while loading keystore and truststore, {}", e);
 		}
+	}
+	
+	public KeyManagerFactory getKeystoreFactory() {
+		return kmf;
+	}
+	
+	public TrustManagerFactory getTrustManagerFactory() {
+		return trustFactory;
 	}
 	
 	public Key getPrivateKey() {
