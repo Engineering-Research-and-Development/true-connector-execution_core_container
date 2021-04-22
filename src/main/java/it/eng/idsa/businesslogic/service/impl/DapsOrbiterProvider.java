e415304ecf176e063af6e0849548bee8a554f77e
igor.balog <igor.balog@eng.it>
2021-04-22 17:26:18 +0200

    Conditional for daps providers

--- src/main/java/it/eng/idsa/businesslogic/service/impl/DapsOrbiterProvider.java
+++ src/main/java/it/eng/idsa/businesslogic/service/impl/DapsOrbiterProvider.java
@@ -16,6 +16,7 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 import com.auth0.jwt.JWT;
@@ -25,6 +26,7 @@ import com.auth0.jwt.interfaces.RSAKeyProvider;
 
 
 @Component
+@ConditionalOnProperty(name = "application.dapsVersion", havingValue = "orbiter")
 public class DapsOrbiterProvider {
     private static final Logger logger = LoggerFactory.getLogger(DapsOrbiterProvider.class);
 
