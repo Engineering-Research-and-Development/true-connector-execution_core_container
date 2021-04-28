6647a37ea73f57d07235347a1d39c36b381579a4
MattiaMarzano-Eng <33173948+MattiaMarzano-Eng@users.noreply.github.com>
2021-04-28 09:19:47 +0200

    update docker compose

--- ci/install.sh
+++ ci/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-DOCKER_COMPOSE_VERSION=1.25.5
+DOCKER_COMPOSE_VERSION=2.2
 
 mkdir $HOME/hash
 mkdir $HOME/cert
 
 echo "Downloading certificate from private repository..."
-git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/tester-sia-rd-eng/private-files-repo.git
+git clone https://${GH_TOKEN}:x-oauth-basic@github.com/tester-sia-rd-eng/private-files-repo.git
 cp -f private-files-repo/*.jks $HOME/cert
 echo "Certificate from private repository downloaded"
 BRANCH_DATA_APP=master
@@ -33,14 +33,14 @@ cp -rf ./ci/.m2/repository/de/fraunhofer/aisec/ids  $HOME/.m2/repository/de/frau
 echo "Installing Multipart Message Lib..."
 git clone https://github.com/Engineering-Research-and-Development/market4.0-ids_multipart_message_processor
 cd market4.0-ids_multipart_message_processor
-mvn clean install -DskipTests
+mvn clean install
 cd ..
 echo "Installed  Multipart Message Lib"
 
 echo "Installing websocket-message-streamer-lib..."
 git clone https://github.com/Engineering-Research-and-Development/market4.0-websocket_message_streamer.git
 cd market4.0-websocket_message_streamer
-mvn clean install -DskipTests
+mvn clean install
 cd ..
 echo "Installed websocket-message-streamer-lib"
 
@@ -48,7 +48,7 @@ echo "Cloning and Creating Docker Container from Data-App repo..."
 git clone https://github.com/Engineering-Research-and-Development/market4.0-data_app_test_BE.git
 cd market4.0-data_app_test_BE
 git checkout ${BRANCH_DATA_APP}
-mvn clean package -DskipTests
+mvn clean package
 docker build -f Dockerfile -t rdlabengpa/data-app .
 cd ..
 echo "Data-App is ready to start"
@@ -56,12 +56,12 @@ echo "Data-App is ready to start"
 echo "Downloading and installing Clearing-House Model..."
 git clone https://github.com/Engineering-Research-and-Development/market4.0-clearing_house.git
 cd market4.0-clearing_house
-mvn install -DskipTests
+mvn install
 cd ..
 echo "Clearing-House Model installed!"
 
 echo "Creating Docker Container for ECCs..."
-mvn clean package -DskipTests
+mvn clean package
 docker build -f Dockerfile -t rdlabengpa/execution_core_container_bl .
 
 echo "Starting services..."
