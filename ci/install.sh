f98a661c0ace15b04f3f7fd754d7618d798d5885
MattiaMarzano-Eng <33173948+MattiaMarzano-Eng@users.noreply.github.com>
2021-05-11 17:43:32 +0200

    set up env

--- ci/install.sh
+++ ci/install.sh
@@ -60,7 +60,7 @@ cd ..
 echo "Clearing-House Model installed!"
 
 echo "Creating Docker Container for ECCs..."
-mvn -U clean install
+mvn -U clean install -DskipTests
 docker build -f Dockerfile -t rdlabengpa/execution_core_container_bl .
 
 #echo "Starting services..."
