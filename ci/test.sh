2a957eab2d49d9d7295f0a2e18c7eaf43b69530c
MattiaMarzano-Eng <33173948+MattiaMarzano-Eng@users.noreply.github.com>
2021-05-11 18:04:07 +0200

    run test

--- ci/test.sh
+++ ci/test.sh
@@ -2,4 +2,31 @@
 # infos at https://learning.postman.com/docs/postman/collection-runs/command-line-integration-with-newman/
 export TEST_TIMEOUT=120000 #2 min in ms
 export ITERATIONS=1
-newman run ./travis/tests/tests-${NET}-${NETE}.json --insecure --timeout-request ${TEST_TIMEOUT} --iteration-count ${ITERATIONS} --bail
+#newman run ./ci/tests/tests.json --insecure --timeout-request ${TEST_TIMEOUT} --iteration-count ${ITERATIONS} --bail
+curl --location --request POST 'https://localhost:8084/proxy' \
+--header 'Content-Type: text/plain' \
+--data-raw '{
+    "multipart": "mixed",
+    "Forward-To": "https://ecc-provider:8889/data",
+	 "message": {
+	  "@context" : {
+		"ids" : "https://w3id.org/idsa/core/"
+	  },
+	  "@type" : "ids:ArtifactRequestMessage",
+	  "@id" : "https://w3id.org/idsa/autogen/artifactRequestMessage/76481a41-8117-4c79-bdf4-9903ef8f825a",
+	  "ids:issued" : {
+		"@value" : "2020-11-25T16:43:27.051+01:00",
+		"@type" : "http://www.w3.org/2001/XMLSchema#dateTimeStamp"
+	  },
+	  "ids:modelVersion" : "4.0.0",
+	  "ids:issuerConnector" : {
+		"@id" : "http://w3id.org/engrd/connector/"
+	  },
+	  "ids:requestedArtifact" : {
+	   "@id" : "http://w3id.org/engrd/connector/artifact/1"
+	  }
+	},
+	"payload" : {
+		"catalog.offers.0.resourceEndpoints.path":"/pet2"
+		}
+}'
