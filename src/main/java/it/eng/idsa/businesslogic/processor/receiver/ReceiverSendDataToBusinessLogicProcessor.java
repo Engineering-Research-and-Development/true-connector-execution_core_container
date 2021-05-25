0c0f8a9c6663cbf202554227b9e255b37615d69e
Vojkan Bukumiric <vojkanbukumiric@gmail.com>
2021-05-25 16:44:15 +0200

    small fix for http-header flow

--- src/main/java/it/eng/idsa/businesslogic/processor/receiver/ReceiverSendDataToBusinessLogicProcessor.java
+++ src/main/java/it/eng/idsa/businesslogic/processor/receiver/ReceiverSendDataToBusinessLogicProcessor.java
@@ -100,7 +100,6 @@ public class ReceiverSendDataToBusinessLogicProcessor implements Processor {
 		headerCleaner.removeTechnicalHeaders(headersParts);
 		logger.info("Sending response to Data Consumer");
 		
-		headerService.removeTokenHeaders(headersParts);
 		exchange.getMessage().setBody(responseString);
 		exchange.getMessage().setHeaders(headersParts);
 	}
