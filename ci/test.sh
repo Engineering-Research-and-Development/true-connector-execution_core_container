077b4f16316938fb80a16a11989f422b01625a06
MattiaMarzano-Eng <33173948+MattiaMarzano-Eng@users.noreply.github.com>
2021-05-31 16:58:03 +0200

    newman test updated

--- ci/test.sh
+++ ci/test.sh
@@ -2,4 +2,4 @@
 # infos at https://learning.postman.com/docs/postman/collection-runs/command-line-integration-with-newman/
 export TEST_TIMEOUT=120000 #2 min in ms
 export ITERATIONS=1
-newman run ./ci/tests/HTTPS-HTTPS.json --insecure --timeout-request ${TEST_TIMEOUT} --iteration-count ${ITERATIONS} --bail
+newman run ./ci/tests/HTTPS-HTTPS.json --timeout-request ${TEST_TIMEOUT} --bail --verbose
