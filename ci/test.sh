222d39758972b61168d0c419920a2327985ddba2
MattiaMarzano-Eng <33173948+MattiaMarzano-Eng@users.noreply.github.com>
2021-06-01 10:02:00 +0200

    adding more tests

--- ci/test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# infos at https://learning.postman.com/docs/postman/collection-runs/command-line-integration-with-newman/
-export TEST_TIMEOUT=120000 #2 min in ms
-export ITERATIONS=1
-newman run ./ci/tests/HTTPS-HTTPS.json --timeout-request ${TEST_TIMEOUT} --insecure --bail --verbose
