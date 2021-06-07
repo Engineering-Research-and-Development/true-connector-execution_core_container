597a33d17ca79f084df2da6b665e8d0594460a9f
MattiaMarzano-Eng <33173948+MattiaMarzano-Eng@users.noreply.github.com>
2021-06-07 12:08:07 +0200

    add deploy

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Deploy to Docker Hub"
-docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
-docker tag rdlabengpa/execution_core_container_bl rdlabengpa/execution_core_container_bl:develop
-docker rmi rdlabengpa/execution_core_container_bl:latest
-docker push rdlabengpa/execution_core_container_bl
+sudo docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
+sudo docker tag rdlabengpa/execution_core_container_bl rdlabengpa/execution_core_container_bl:develop
+sudo docker rmi rdlabengpa/execution_core_container_bl:latest
+sudo docker push rdlabengpa/execution_core_container_bl
