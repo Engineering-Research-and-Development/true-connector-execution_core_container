6abb1d2d7edfd13b08db32f6fcfde168f4c16cca
MattiaMarzano-Eng <33173948+MattiaMarzano-Eng@users.noreply.github.com>
2021-06-10 14:41:20 +0200

    update deploy GHA

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Deploy to Docker Hub for developer"
 sudo docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
-sudo docker tag rdlabengpa/execution_core_container_bl rdlabengpa/execution_core_container_bl:develop
-sudo docker push rdlabengpa/execution_core_container_bl
+sudo docker tag rdlabengpa/execution_core_container_bl rdlabengpa/ids_execution_core_container:develop
+sudo docker push rdlabengpa/ids_execution_core_container:develop
