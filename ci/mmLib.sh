2402130a17cbb45b0139fb0aab53f7a7fe2fdf54
David Jovanovic <david.jovanovic@eng.it>
2021-10-08 10:32:17 +0200

    changed GHA for development testing

--- ci/mmLib.sh
+++ ci/mmLib.sh
@@ -1,5 +1,5 @@
 echo "Installing Multipart Message Lib..."
-git clone https://github.com/Engineering-Research-and-Development/true-connector-multipart_message_library.git
+git clone -b TestUtilMessageService_removal https://github.com/Engineering-Research-and-Development/true-connector-multipart_message_library.git
 cd true-connector-multipart_message_library
 mvn -U clean install
 cd ..
