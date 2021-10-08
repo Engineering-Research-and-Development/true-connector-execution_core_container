a625d5f8c081f12aa8fe681a110c532f5e51c598
David Jovanovic <david.jovanovic@eng.it>
2021-10-08 15:56:25 +0200

    reverted development changes for GHA workflow

--- ci/wmsLib.sh
+++ ci/wmsLib.sh
@@ -1,5 +1,5 @@
 echo "Installing websocket-message-streamer-lib..."
-git clone -b TestUtilMessageService_removal https://github.com/Engineering-Research-and-Development/true-connector-websocket_message_streamer.git
+git clone https://github.com/Engineering-Research-and-Development/true-connector-websocket_message_streamer.git
 cd true-connector-websocket_message_streamer
 mvn -U clean install
 cd ..
