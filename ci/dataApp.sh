134793255ac626630b61d921372112a5c2519076
Petar Crepulja <petar.crepulja@eng.it>
2021-11-15 12:10:55 +0100

    Reverted to default

--- ci/dataApp.sh
+++ ci/dataApp.sh
@@ -1,5 +1,5 @@
 echo "Cloning and Creating Docker Container from Data-App repo..."
-git clone -b proxy_wss_fix https://github.com/Engineering-Research-and-Development/true-connector-basic_data_app.git
+git https://github.com/Engineering-Research-and-Development/true-connector-basic_data_app.git
 cd true-connector-basic_data_app
 mvn -U clean install
 sudo docker build -f Dockerfile -t rdlabengpa/ids_be_data_app:latest .
