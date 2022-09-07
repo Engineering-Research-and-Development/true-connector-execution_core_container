d68db0da1e8201918c9ca7f8d36d2cb1e87078ee
igor.balog <igor.balog@eng.it>
2022-09-07 12:35:20 +0200

    processor.sender.websocket.client tests

---
+++ src/test/java/it/eng/idsa/businesslogic/processor/sender/websocket/client/FileStreamingBeanTest.java
@@ -0,0 +1,50 @@
+package it.eng.idsa.businesslogic.processor.sender.websocket.client;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ExecutionException;
+
+import org.asynchttpclient.ws.WebSocket;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import it.eng.idsa.businesslogic.configuration.WebSocketClientConfiguration;
+import it.eng.idsa.businesslogic.util.MultipartMessageUtil;
+
+public class FileStreamingBeanTest {
+
+	private static final String START_BINARY_FRAME_SEPARATOR = "�normal-IDS-ENG-SEPARATOR the-first-frame";
+
+	private static final String END_BINARY_FRAME_SEPARATOR = "�normal-IDS-ENG-SEPARATOR the-last-frame";
+
+	@InjectMocks
+	private FileStreamingBean streamingBean;
+	
+	@Mock
+	private WebSocketClientConfiguration webSocketClientConfiguration;
+	@Mock
+	private WebSocket webSocket;
+	
+	@BeforeEach
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		streamingBean.setup(webSocket);
+	}
+	
+	@Test
+	public void sendMultipartMessage() throws KeyManagementException, NoSuchAlgorithmException, InterruptedException, ExecutionException, IOException {
+		when(webSocket.isOpen()).thenReturn(true);
+		
+		streamingBean.sendMultipartMessage(MultipartMessageUtil.getMultipartMessageAsString());
+		
+		verify(webSocket).sendBinaryFrame(START_BINARY_FRAME_SEPARATOR.getBytes(), false, 0);
+		verify(webSocket).sendBinaryFrame(END_BINARY_FRAME_SEPARATOR.getBytes(), false, 0);
+	}
+}
---
+++ src/test/java/it/eng/idsa/businesslogic/processor/sender/websocket/client/ResponseMessageReceiverClientTest.java
@@ -0,0 +1,40 @@
+package it.eng.idsa.businesslogic.processor.sender.websocket.client;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.asynchttpclient.ws.WebSocket;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import it.eng.idsa.businesslogic.configuration.WebSocketClientConfiguration;
+
+public class ResponseMessageReceiverClientTest {
+
+	@InjectMocks
+	private ResponseMessageReceiverClient client;
+	@Mock
+	private WebSocketClientConfiguration webSocketClientConfiguration;
+	@Mock
+	private ResponseMessageBufferClient bufferClient;
+	@Mock
+	private WebSocket wsClient;
+	
+	@BeforeEach
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		client.setup(wsClient);
+		when(webSocketClientConfiguration.responseMessageBufferWebSocketClient()).thenReturn(bufferClient);
+		when(bufferClient.remove()).thenReturn("MESSAGE".getBytes());
+	}
+	
+	@Test
+	public void run() {
+		client.run();
+		
+		verify(wsClient).sendCloseFrame(200, "Shutdown");
+	}
+}
