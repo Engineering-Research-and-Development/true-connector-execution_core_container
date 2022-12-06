e1a0a7e7808c794db42108a19a8417f0b23c1459
igor.balog <igor.balog@eng.it>
2022-12-06 11:43:52 +0100

    GitHub Actions update

--- ci/ch.sh
+++
@@ -1,6 +0,0 @@
-echo "Downloading and installing Clearing-House Model..."
-git clone -b infomodel_4.0.6 https://github.com/Engineering-Research-and-Development/market4.0-clearing_house.git
-cd market4.0-clearing_house
-mvn install -DskipTests
-cd ..
-echo "Clearing-House Model installed!"
