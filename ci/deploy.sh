f9796cea35ab82bbe57732eb9199b4def5b94e2d
David Jovanovic <david.jovanovic@eng.it>
2022-12-28 11:56:33 +0100

    added  setup env to build

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Creating Docker Image from ECC repo..."
-sudo docker build -f Dockerfile -t rdlabengpa/ids_execution_core_container:develop .
+sudo docker build -f Dockerfile -t rdlabengpa/ids_execution_core_container:test .
 cd ..
 echo "ECC is ready"
 echo "Starting deployment to Docker Hub"
 sudo docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
-sudo docker push rdlabengpa/ids_execution_core_container:develop
+sudo docker push rdlabengpa/ids_execution_core_container:test
 echo "ECC deployed to Docker Hub"
