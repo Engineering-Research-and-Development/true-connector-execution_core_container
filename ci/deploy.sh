49374ec7e34cc479d106ae3d0b60bb2ed0f8f013
David Jovanovic <david.jovanovic@eng.it>
2022-12-28 11:24:35 +0100

    added gh token to test env

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,8 +3,8 @@
 echo "Creating Docker Image from ECC repo..."
 sudo docker build -f Dockerfile -t rdlabengpa/ids_execution_core_container:develop .
 cd ..
-echo "Data-App is ready"
+echo "ECC is ready"
 echo "Starting deployment to Docker Hub"
 sudo docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
 sudo docker push rdlabengpa/ids_execution_core_container:develop
-echo "Data-App deployed to Docker Hub"
+echo "ECC deployed to Docker Hub"
