91dbff85e4676fbf2be724d8427d4375e43cd0c1
David Jovanovic <david.jovanovic@eng.it>
2022-12-28 10:19:27 +0100

    mixed and form tests

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo "Deploy to Docker Hub for developer"
+
+echo "Creating Docker Image from ECC repo..."
+sudo docker build -f Dockerfile -t rdlabengpa/ids_execution_core_container:develop .
+cd ..
+echo "Data-App is ready"
+echo "Starting deployment to Docker Hub"
 sudo docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
-sudo docker tag rdlabengpa/execution_core_container_bl rdlabengpa/ids_execution_core_container:develop
 sudo docker push rdlabengpa/ids_execution_core_container:develop
+echo "Data-App deployed to Docker Hub"
