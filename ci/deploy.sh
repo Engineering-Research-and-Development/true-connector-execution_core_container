f19e92494d8c36580d12f8bcf03decf3f3564a78
David Jovanovic <david.jovanovic@eng.it>
2022-12-29 11:55:05 +0100

    changed version to develop

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Creating Docker Image from ECC repo..."
-sudo docker build -f Dockerfile -t rdlabengpa/ids_execution_core_container:test .
+sudo docker build -f Dockerfile -t rdlabengpa/ids_execution_core_container:develop .
 cd ..
 echo "ECC is ready"
 echo "Starting deployment to Docker Hub"
 sudo docker login -u ${DOCKER_USER} -p ${DOCKER_PASSWORD}
-sudo docker push rdlabengpa/ids_execution_core_container:test
+sudo docker push rdlabengpa/ids_execution_core_container:develop
 echo "ECC deployed to Docker Hub"
