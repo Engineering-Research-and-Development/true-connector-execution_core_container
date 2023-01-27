aeca58dffbe8c16ede7981bc7a1ade04c52b0be5
gianluca-isgro <11504420+gianluca-isgro@users.noreply.github.com>
2023-01-27 12:54:58 +0100

    Update OkHttpClientConfiguration.java
    Deleted unuseful import

--- src/main/java/it/eng/idsa/businesslogic/configuration/OkHttpClientConfiguration.java
+++ src/main/java/it/eng/idsa/businesslogic/configuration/OkHttpClientConfiguration.java
@@ -3,7 +3,6 @@ package it.eng.idsa.businesslogic.configuration;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
-import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 import javax.net.ssl.HostnameVerifier;
@@ -22,7 +21,6 @@ import org.springframework.context.annotation.Configuration;
 
 import it.eng.idsa.businesslogic.service.impl.KeystoreProvider;
 import okhttp3.ConnectionSpec;
-import okhttp3.OkHttpClient;
 import okhttp3.internal.tls.OkHostnameVerifier;
 
 @Configuration
