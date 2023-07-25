9448d2ca25c746793ef436cf4e90c41f71525c04
Marko Stojanovic <stojanovic1994@yahoo.com>
2023-07-25 15:07:16 +0200

    Clean left over properties, and fix ApplicationTests

--- src/test/java/it/eng/idsa/businesslogic/ApplicationTests.java
+++ src/test/java/it/eng/idsa/businesslogic/ApplicationTests.java
@@ -3,10 +3,14 @@ package it.eng.idsa.businesslogic;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(properties = { "server.ssl.enabled=true", "camel.springboot.main-run-controller=true",
+		"camel.component.http4.use-global-ssl-context-parameters=true", "ssl.enabled=true",
+		"application.disableSslVerification=false", })
 public class ApplicationTests {
 
 	@Test
