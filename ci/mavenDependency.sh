ad7c865e14831c7a860dba1dd989d31f34d681f7
Marko Stojanovic <stojanovic1994@yahoo.com>
2023-11-09 14:00:48 +0100

    Fix recursive copy in script

--- ci/mavenDependency.sh
+++ ci/mavenDependency.sh
@@ -5,4 +5,4 @@ echo "Installing maven dependencies..."
 mkdir -p $HOME/.m2/de
 
 cp -f ./ci/.m2/settings/settings.xml  $HOME/.m2
-cp -f ./ci/.m2/de/*  $HOME/.m2/de/
+cp -rf ./ci/.m2/de/*  $HOME/.m2/de/
