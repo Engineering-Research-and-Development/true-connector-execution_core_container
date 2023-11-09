963f284e509ba9b83dae836359734e0fbdbb26b6
Marko Stojanovic <stojanovic1994@yahoo.com>
2023-11-09 14:29:09 +0100

    Fix script

--- ci/mavenDependency.sh
+++ ci/mavenDependency.sh
@@ -2,7 +2,7 @@
 
 echo "Installing maven dependencies..."
 
-mkdir -p $HOME/.m2/de
+mkdir -p $HOME/.m2/repository/de
 
-cp -f ./ci/.m2/settings/settings.xml  $HOME/.m2/repository
+cp -f ./ci/.m2/settings/settings.xml  $HOME/.m2
 cp -rf ./ci/.m2/de/*  $HOME/.m2/repository/de/
