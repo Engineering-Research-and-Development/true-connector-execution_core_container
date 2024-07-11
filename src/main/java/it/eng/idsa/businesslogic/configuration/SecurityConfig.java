60f061971d4ed4db8087420fc9018c1d165f850d
marest94 <stojanovic1994@yahoo.com>
2024-07-11 14:55:06 +0200

    Modify SecurityConfig to use CORS config from property file

--- src/main/java/it/eng/idsa/businesslogic/configuration/SecurityConfig.java
+++ src/main/java/it/eng/idsa/businesslogic/configuration/SecurityConfig.java
@@ -1,6 +1,10 @@
 package it.eng.idsa.businesslogic.configuration;
-	
+
+import java.util.Arrays;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,6 +16,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import it.eng.idsa.businesslogic.util.TrueConnectorConstants;
 
@@ -25,34 +32,35 @@ import it.eng.idsa.businesslogic.util.TrueConnectorConstants;
 @EnableWebSecurity
 @EnableScheduling
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Value("${application.cors.allowed.origins:}")
+	private String allowedOrigins;
+
+	@Value("${application.cors.allowed.methods:}")
+	private String allowedMethods;
+
+	@Value("${application.cors.allowed.headers:}")
+	private String allowedHeaders;
+	
 	@Autowired
 	private UserDetailsService inMemoryUserCrudService;
-	
+
 	@Override
-    public void configure(WebSecurity web) throws Exception {
+	public void configure(WebSecurity web) throws Exception {
 		// allow Swagger UI to be displayed without asking credentials in browser
-        web.ignoring().antMatchers("/swagger-ui.html", "/swagger-ui/**");
-    }
-	 
+		web.ignoring().antMatchers("/swagger-ui.html", "/swagger-ui/**");
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.userDetailsService(inMemoryUserCrudService)
-			.cors()
-			.and()
-			.sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and()
-			.authorizeRequests()
-			.antMatchers("/about/**").permitAll()
-			.antMatchers("/error").permitAll()
-			.antMatchers("/").permitAll()
-			.antMatchers("/api/**").hasRole(TrueConnectorConstants.API_USER_ROLE)
-			.anyRequest().authenticated()
-			.and()
-			.csrf().disable()
-			.httpBasic()
-			.authenticationEntryPoint(authenticationEntryPoint());
+		http.cors().and()
+		.csrf().disable()
+		.httpBasic().authenticationEntryPoint(authenticationEntryPoint()).and()
+		.userDetailsService(inMemoryUserCrudService).sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
+		.antMatchers("/about/**").permitAll().antMatchers("/error").permitAll().antMatchers("/").permitAll()
+		.antMatchers("/api/**").hasRole(TrueConnectorConstants.API_USER_ROLE).anyRequest().authenticated();
+
 		http.headers().xssProtection();
 	}
 
@@ -61,5 +69,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		final var entryPoint = new BasicAuthenticationEntryPoint();
 		entryPoint.setRealmName("api realm");
 		return entryPoint;
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		if (StringUtils.isBlank(allowedOrigins)) {
+			configuration.addAllowedOrigin("*");
+		} else {
+			configuration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
+		}
+
+		if (StringUtils.isBlank(allowedMethods)) {
+			configuration.addAllowedMethod("*");
+		} else {
+			configuration.setAllowedMethods(Arrays.asList(allowedMethods.split(",")));
+		}
+
+		if (StringUtils.isBlank(allowedHeaders)) {
+			configuration.addAllowedHeader("*");
+		} else {
+			configuration.setAllowedHeaders(Arrays.asList(allowedHeaders.split(",")));
+		}
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
